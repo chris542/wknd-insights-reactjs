b1174a756c03c4f4b464ccc7848642adc7f76253
Chris Kang <chrisk@movio.co>
2018-12-20 16:32:44 +1300

    UPDATE: fake database to be more suitable for real case

--- src/components/intro.js
+++ src/components/intro.js
@@ -2,23 +2,23 @@ import React from 'react';
 import Calendar from './intro-calendar-btn';
 import Twitter from './intro-twitter';
 
-const Intro = () => {
-    const description = "Elit a laboriosam quos expedita magnam incidunt soluta? Dolores repellendus ut nihil molestiae asperiores, sequi Quae re";
-
+const Intro = ({insights}) => {
     return (
+        insights?
         <div className="wknd-insights-intro">
             <h1>Weekend Insights</h1>
             <div className="wknd-insights-info grey-bg">
                 <Calendar/>
                 <div className="container">
-                    <h2>23 - 25 Nov 2018</h2>
+                    <h2>{insights.title}</h2>
                     <div className="body">
-                        <p>{description}</p>
+                        <p>{insights.body_text}</p>
                     </div>
                     <Twitter/>
                 </div>
             </div>
         </div>
+        :null
     )
 
 }
